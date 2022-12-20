9103cc08e3c9742cfc942d136d81ceb742725626
suraj keshri <surajthekeshri@gmail.com>
2022-12-21 01:39:46 +0530

    chnage

---
+++ database/migrations/2022_12_20_200115_create_blogs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id('blog_id',false);
+            $table->string('blog_title');
+            $table->text('blog_desc');
+            $table->string('blog_image');
+            $table->string('blog_catogery');
+            $table->string('user_id');
+            $table->boolean('blog_status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
